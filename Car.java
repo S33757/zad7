e45f9d778f6e064f5ae2367d258463fe251e6d64
Hubert Bogucki <s33757@pjwstk.edu.pl>
2025-05-17 20:20:23 +0200

    zadanie2

--- Car.java
+++ Car.java
@@ -2,6 +2,16 @@ public class Car extends Vehicle {
 
     private int numerOfSeats;
 
+    @Override
+    public void start() {
+        System.out.println("Samochod zostal wlaczony");
+    }
+
+    @Override
+    public void stop(){
+        System.out.println("Samochod zostal zatrzymany");
+    }
+
     public Car(int numerOfSeats) {
         this.numerOfSeats = numerOfSeats;
     }
